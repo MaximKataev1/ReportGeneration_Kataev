4a25e07cbe2f212fa8cc82e55be6ba59ba4f03ef
maxim <maxim@DESKTOP-C3IICAN>
2025-10-01 03:08:47 +0500

    Программирование пользовательского элемента Student. Kataev

--- ReportGeneration_Kataev/Items/Student.xaml.cs
+++ ReportGeneration_Kataev/Items/Student.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ReportGeneration_Kataev.Classes;
+using ReportGeneration_Kataev.Models;
+using ReportGeneration_Kataev.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +23,54 @@ namespace ReportGeneration_Kataev.Items
     /// </summary>
     public partial class Student : UserControl
     {
+
         public Student()
         {
+            TBFio.Text = $"{student.Lastname} {student.Firstname}";
+            CBExpelled.IsChecked = student.Expelled;
+            List<DisciplineContext> StudentDisciplines = Main.AllDisciplines.FindAll(
+                x => x.IdGroup == student.IdGroup);
+
+            int NecessarilyCount = 0;
+            int WorksCount = 0;
+            int DoneCount = 0;
+            int MissedCount = 0;
+
+            foreach (DisciplineContext StudentDiscipline in StudentDisciplines)
+            {
+                List<WorkContext> StudentWorks = Main.AllWorks.FindAll(x =>
+                (x.IdType == 1 || x.IdType == 2 || x.IdType == 3) &&
+                x.IdDiscipline == StudentDiscipline.Id);
+                NecessarilyCount += StudentWorks.Count;
+
+                foreach (WorkContext StudentWork in StudentWorks)
+                {
+                    EvaluationContext Evaluation = Main.AllEvaluation.Find(x =>
+                    x.IdWork == StudentWork.Id &&
+                    x.IdStudent == student.Id);
+                    if (Evaluation != null && Evaluation.Value.Trim() != "" && Evaluation.Value.Trim() != "2")
+                        DoneCount++;
+                }
+
+                StudentWorks = Main.AllWorks.FindAll(x =>
+                    x.IdType != 4 && x.IdType != 3);
+                WorksCount += StudentWorks.Count;
+
+                foreach (WorkContext StudentWork in StudentWorks)
+                {
+                    EvaluationContext Evaluation = Main.AllEvaluation.Find(x =>
+                    x.IdWork == StudentWork.Id &&
+                    x.IdStudent == student.Id);
+                    if (Evaluation != null && Evaluation.Lateness.Trim() != "")
+                        MissedCount += Convert.ToInt32(Evaluation.Lateness);
+                }
+            }
+
+            doneWorks.Value = (100f / (float)NecessarilyCount) * ((float)DoneCount);
+            missedCount.Value = (100f / ((float)WorksCount * 90f)) * ((float)MissedCount);
+            TBGroup.Text = Main.AllGroups.Find(x => x.Id == student.IdGroup).Name;
             InitializeComponent();
         }
 
-        private void TBFio_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
